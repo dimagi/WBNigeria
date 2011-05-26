fe2f3e72a0aa6c255bbbe2a89f4002a50e96ac04
Dan Poirier <dpoirier@caktusgroup.com>
2011-05-26 08:07:51 -0400

    Show daily doses field when creating or editing a patient.

--- aremind/apps/patients/forms.py
+++ aremind/apps/patients/forms.py
@@ -81,7 +81,8 @@ class PatientRemindersForm(forms.ModelForm):
 
     class Meta(object):
         model = patients.Patient
-        fields = ('subject_number', 'first_name', 'last_name', 'mobile_number', 'pin', 'next_visit', 'reminder_time', )
+        fields = ('subject_number', 'first_name', 'last_name', 'mobile_number',
+                  'pin', 'next_visit', 'reminder_time', 'daily_doses')
 
     def __init__(self, *args, **kwargs):
         super(PatientRemindersForm, self).__init__(*args, **kwargs)

08cdc959192a4afbc66c32cc498153fa8d6e7da1
Anton de Winter <adewinter@dimagi.com>
2012-03-23 11:37:53 -0400

    Registration app, xform entry session now works on multiple runs
    Signed-off-by: Anton de Winter <adewinter@dimagi.com>

--- aremind/apps/wbn_registration/models.py
+++ aremind/apps/wbn_registration/models.py
@@ -7,3 +7,6 @@ class WBUser(models.Model):
     connection = models.ForeignKey(Connection)
     is_registered = models.BooleanField(default=False)
     location_code = models.CharField(max_length=4)
+
+    def __unicode__(self):
+        return 'Connection: %s, Is Registered?: %s, Location Code: %s'  % (self.connection, self.is_registered, self.location_code)

f0fabd04eae4ecfce5c907c0203451e319f541f6
Anton de Winter <adewinter@dimagi.com>
2012-04-02 00:24:09 +0100

    Moved rapidsms-smsforms (previously smstouchforms) code to it's own repo @ dimagi/rapidsms-smsforms
    Signed-off-by: Anton de Winter <adewinter@dimagi.com>

--- aremind/apps/rapidsms-smsforms/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls.defaults import *
-from aremind.apps.smstouchforms import views
-
-
-urlpatterns = patterns('',
-    url(r'^edit/$', views.edit_triggers, name='edit-triggers'),
-    url(r'^view/$', views.view_triggers, name='view-triggers'),
-    )

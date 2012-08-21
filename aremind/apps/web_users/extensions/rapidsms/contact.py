d6042ed78c8bbaf721c8ebc5e179c44387ae571f
Rebecca Lovewell <rebecca@caktusgroup.com>
2012-08-21 11:37:51 -0400

    Added unique constraint on Contact.user

--- aremind/apps/web_users/extensions/rapidsms/contact.py
+++ aremind/apps/web_users/extensions/rapidsms/contact.py
@@ -6,7 +6,7 @@ from rapidsms.contrib.locations.models import Location
 
 class ContactExtra(models.Model):
     """Extensions to the RapidSMS contact model to locate a user."""
-    user = models.ForeignKey(User, null=True, blank=True)
+    user = models.ForeignKey(User, null=True, blank=True, unique=True)
     location = models.ForeignKey(Location, null=True, blank=True)
 
     class Meta:

72991312b77e79a5c74fe332b2433016b0d1a34f
Rebecca Lovewell <rebecca@caktusgroup.com>
2012-08-21 14:25:23 -0400

    Added fixtures for locations data

--- aremind/migrations/rapidsms/0006_auto__add_field_contact_user__add_field_contact_location.py
+++ aremind/migrations/rapidsms/0006_auto__add_field_contact_user__add_field_contact_location.py
@@ -7,6 +7,10 @@ from django.db import models
 
 class Migration(SchemaMigration):
 
+    depends_on = (
+        ("locations", "0002_auto__add_field_location_name__add_field_location_slug"),
+    )
+
     def forwards(self, orm):
         # Adding field 'Contact.user'
         db.add_column('rapidsms_contact', 'user',

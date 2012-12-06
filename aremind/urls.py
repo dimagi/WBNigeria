ebc962d7b4ce74cf8b323c78995beff7e9b8e5fb
bayo <bayo@codebench.(none)>
2012-12-06 20:08:53 +0100

    new urls for login/logout + welcome

--- aremind/urls.py
+++ aremind/urls.py
@@ -19,8 +19,11 @@ urlpatterns = patterns('',
 #    (r'^', include('decisiontree.urls')),
 
     # RapidSMS core URLs
+    url(r'^account/login/', 'rapidsms.views.login', name='rapidsms_login'),
+    url(r'^account/logout/', 'rapidsms.views.logout', name='rapidsms_logout'),
     (r'^account/', include('rapidsms.urls.login_logout')),
     url(r'old/$', 'aremind.apps.broadcast.views.dashboard', name='broadcast-dashboard'),
+    url(r'^welcome/$', 'aremind.apps.web_users.views.welcome'),
     url(r'^$', 'smscouchforms.views.download', name='rapidsms-dashboard'),
 
     url(r'^settings/$', direct_to_template,
@@ -74,11 +77,11 @@ if 'auditcare' in settings.INSTALLED_APPS:
 
 
  # Contrib Auth Password Management
-urlpatterns += patterns('django.contrib.auth.views',   
+urlpatterns += patterns('django.contrib.auth.views',
     url(r'^account/password/change/$', 'password_change', name='auth_password_change'),
     url(r'^account/password/change/done/$', 'password_change_done', name='auth_password_change_done'),
     url(r'^account/password/reset/$', 'password_reset', name='auth_password_reset'),
-    url(r'^account/password/reset/confirm/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$', 'password_reset_confirm', 
+    url(r'^account/password/reset/confirm/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$', 'password_reset_confirm',
         name='auth_password_reset_confirm'),
     url(r'^account/password/reset/complete/$', 'password_reset_complete', name='auth_password_reset_complete'),
     url(r'^account/password/reset/done/$', 'password_reset_done', name='auth_password_reset_done'),

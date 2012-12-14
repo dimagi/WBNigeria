6654c47fbe72a04782fe3d5b3e843ddf3aa0b935
Drew Roos <droos@dimagi.com>
2012-12-14 16:19:13 +0200

    fix fadama main dashboard

--- aremind/apps/dashboard/utils/fadama.py
+++ aremind/apps/dashboard/utils/fadama.py
@@ -153,7 +153,7 @@ def main_dashboard_stats(user_state):
             'total': len(data),
             'satisfaction': map_reduce(data, lambda r: [(r['satisfied'],)], len),
             'by_category': dict((k, len([r for r in data if k in r])) for k in COMPLAINT_TYPES),
-            'by_clinic': [[facilities[k], v] for k, v in map_reduce(data, lambda r: [(r['facility'],)], len).iteritems()],
+            'by_clinic': [[facilities.get(k), v] for k, v in map_reduce(data, lambda r: [(r['facility'],)], len).iteritems()],
             'month': label[0],
             '_month': label[1],
         }
